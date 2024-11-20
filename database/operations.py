a46dab9a67a179b8e57bf59806c6defa0e5d88f4
Kristian <kristianhnielsen@protonmail.com>
2024-11-20 13:29:38 +0100

    add db operations

---
+++ database/operations.py
@@ -0,0 +1,32 @@
+from sqlalchemy import create_engine, Engine
+from sqlalchemy.orm import Session
+from database.models import Base
+from database.utils import (
+    create_price_objects,
+    create_product_object,
+    remove_duplicates,
+)
+
+
+def process_product_data(product_data, session: Session):
+    product_objs = [create_product_object(product) for product in product_data]
+    products = remove_duplicates(product_objs, session)
+
+    price_objs = create_price_objects(product_data)
+    prices = remove_duplicates(price_objs, session)
+
+    return products, prices
+
+
+def add_products(data):
+    engine: Engine = create_engine("sqlite:///rema.db", echo=True)
+    with Session(engine) as session:
+        Base.metadata.create_all(engine)
+
+        products, prices = process_product_data(data, session)
+
+        session.add_all(products)
+        session.add_all(prices)
+        session.commit()
+
+        print("done")
