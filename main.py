5cfae2eca761aafba6d6036b012bb56caf83403b
Kristian <kristianhnielsen@protonmail.com>
2024-11-29 16:37:42 +0100

    include prices router

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from routers.products import router as products_router
+from routers.prices import router as prices_router
 
 
 app = FastAPI()
 
 app.include_router(products_router)
+app.include_router(prices_router)
 
 
 @app.get("/")
