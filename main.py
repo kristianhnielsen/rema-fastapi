6dfe7ff15679c69cfd212a8fb43b0840437f18d2
Kristian <kristianhnielsen@protonmail.com>
2024-12-09 12:00:04 +0100

    add cors middleware

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from routers.products import router as products_router
 from routers.prices import router as prices_router
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(products_router)
 app.include_router(prices_router)
